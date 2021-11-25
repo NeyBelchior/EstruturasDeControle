24ccaa0a7fa52d621079580a6c57bed0841523ff
Ney Belchior <neybelchior@nomus.com.br>
2021-11-25 07:18:43 -0300

    If

--- If.java
+++ If.java
@@ -1,10 +1,40 @@
 package estruturaDeControle;
 
+import java.util.Scanner;
+
 public class If {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Informe a média :");
+		double media  = entrada.nextDouble();
+		
+		// Obrigatoriamente deve retorna true or false , não converte n nada em boleano, java é mais restrito 
+		if(media<=10&&media>=7) {
+			System.out.println("Aprovado!");
+			System.out.println("Prabéns!");
+			}
+		if(media<7 && media >=4.5) {
+			System.out.println("Recuperação");
+			
+		}
+		
+	    if(media >=4.0 && media >=0) {
+	    	System.out.println("Reprovado");
+	    	
+	    	
+	    }
+	    
+			
+			
+		
+		
+		
+		
+		
+		entrada.close();
 	}
 
 }
